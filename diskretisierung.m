36207de283c90444769c507fb3486a5029277d35
Lennart Duvenbeck <duvenblt@stud44.mathematik.uni-stuttgart.de>
2019-05-28 12:35:18 +0200

    Gerinfügige Updates

--- diskretisierung.m
+++ diskretisierung.m
@@ -2,6 +2,3 @@ function discr = diskretisierung(n, m)
 discr.Xh = random_center(n, m);
 discr.Xh_strich = discr.Xh;
 discr.gamma = 1;
-discr.last_inner = n;
-discr.last_dirichlet = n+m;
-discr.size = n+m;
--- solution.m
+++ solution.m
@@ -1,5 +1,5 @@
-n = 0;
-m = 10;
+n = 500;
+m = 200;
 discr = diskretisierung(n, m);
 model = praktikum_poisson_model();
 
@@ -7,7 +7,8 @@ plot(discr.Xh(1:n, 1), discr.Xh(1:n, 2), 'r.');
 hold on
 plot(discr.Xh(n+1:end,1), discr.Xh(n+1:end,2), 'b.');
 
-glob = discr.Xh_strich;
+[glob, last_inner, last_dirichlet] = point_sorter(discr.Xh_strich, model);
+glob
 
 [A, b] = lgs_assembler(model, discr);
 
@@ -30,4 +31,23 @@ b1 = model.source(discr.Xh_strich(1:discr.last_inner,:));
 b2 = model.dirichlet_values(discr.Xh_strich(discr.last_inner+1:discr.last_dirichlet,:));
 b3 = model.neumann_values(discr.Xh_strich(discr.last_dirichlet+1:end,:));
 b = [b1; b2; b3];
+end
+
+function [sorted_list, last_inner, last_dirichlet] = point_sorter(glob, model)
+temp_inner = [];
+temp_dirichlet = [];
+temp_neumann = [];
+for x = glob
+    switch model.boundary_type(x)
+        case 0
+            temp_inner.append(x)
+        case -1
+            temp_dirichlet.append(x)
+        case -2
+            temp_neumann.append(x)
+    end
+end
+last_inner = size(temp_inner(1));
+last_dirichlet = size(temp_dirichlet(1)) + last_inner;
+sorted_list = [temp_inner; temp_dirichlet; temp_neumann];
 end
