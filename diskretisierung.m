3bbb511885fa7aac5d2865d8dc386a45b4997479
Lennart <lennart.duvenbeck@gmail.com>
2019-06-02 23:11:04 +0200

    Grad, Div erstellt und Sortierer für glob; TODO Assemblierung richtig umsetzen und vektorisieren

--- diskretisierung.m
+++ diskretisierung.m
@@ -2,3 +2,22 @@ function discr = diskretisierung(n, m)
 discr.Xh = random_center(n, m);
 discr.Xh_strich = discr.Xh;
 discr.gamma = 1;
+discr.ds = 1e-5;
+discr.grad = @gradient;
+discr.div = @divergence;
+discr.eval = @evaluation;
+
+    function res = gradient(f, x)
+        res = [(f(x+[discr.ds/2,0])-f(x-[discr.ds/2,0]))/discr.ds,(f(x+[0,discr.ds/2])-f(x-[0,discr.ds/2]))/discr.ds];
+    end
+    function res = divergence(f, x)
+        temp1 = f(x+[discr.ds/2, 0]);
+        temp2 = f(x-[discr.ds/2,0]);
+        temp3 = f(x+[0,discr.ds/2]);
+        temp4 = f(x-[0,discr.ds/2]);
+        res = (temp1(:,1)-temp2(:,1))/discr.ds+(temp3(:,2)-temp4(:,2))/discr.ds;
+    end
+    function res = evaluation(x)
+        res = exp(-discr.gamma *((x(1) - discr.Xh(:,1)).^2 + (x(2) - discr.Xh(:,2)).^2));
+    end
+end
--- solution.m
+++ solution.m
@@ -1,53 +1,44 @@
 n = 500;
 m = 200;
 discr = diskretisierung(n, m);
-model = praktikum_poisson_model();
-
-plot(discr.Xh(1:n, 1), discr.Xh(1:n, 2), 'r.');
-hold on
-plot(discr.Xh(n+1:end,1), discr.Xh(n+1:end,2), 'b.');
+model = praktikum_elliptisch_model(0.5);
 
 [glob, last_inner, last_dirichlet] = point_sorter(discr.Xh_strich, model);
-glob
+disp(last_inner);
+disp(last_dirichlet);
 
-[A, b] = lgs_assembler(model, discr);
+plot(glob(1:last_inner, 1), glob(1:last_inner, 2), 'r.');
+hold on
+plot(glob(last_inner+1:last_dirichlet,1), glob(last_inner+1:last_dirichlet,2), 'b.');
+hold on
+plot(glob(last_dirichlet+1:end,1), glob(last_dirichlet+1:end,2), 'g.');
 
-function res = evaluation(x, discr)
-res = exp(-discr.gamma *((x(1) - discr.Xh(:,1)).^2 + (x(2) - discr.Xh(:,2)).^2));
-end
+[A, b] = lgs_assembler(model, discr, last_inner, last_dirichlet);
 
-function [A, b] = lgs_assembler(model, discr)
-A = zeros(discr.size, discr.size);
-for i = 1:discr.last_inner
+function [A, b] = lgs_assembler(model, discr, last_inner, last_dirichlet)
+A = zeros(length(discr.Xh),length(discr.Xh_strich));
+for i = 1:last_inner
     TODO
 end
-for i = discr.last_inner+1:discr.last_dirichlet
-    A(i,:) = evaluation(discr.Xh_strich(i,:), discr);
+for i = last_inner+1:last_dirichlet
+    A(i,:) = discr.eval(discr.Xh_strich(i,:));
 end
-for i = discr.last_dirichlet+1:discr.size
-    TODO
+for i = last_dirichlet+1:length(discr.Xh_strich)
+    A(i,:) = model.diffusivity(discr.Xh_strich(i,:))*discr.grad(discr.eval, discr.Xh_strich(i,:))*model.normals(discr.Xh_strich(i,:));
 end
-b1 = model.source(discr.Xh_strich(1:discr.last_inner,:));
-b2 = model.dirichlet_values(discr.Xh_strich(discr.last_inner+1:discr.last_dirichlet,:));
-b3 = model.neumann_values(discr.Xh_strich(discr.last_dirichlet+1:end,:));
+b1 = model.source(discr.Xh_strich(1:last_inner,:));
+b2 = model.dirichlet_values(discr.Xh_strich(last_inner+1:last_dirichlet,:));
+b3 = model.neumann_values(discr.Xh_strich(last_dirichlet+1:end,:));
 b = [b1; b2; b3];
 end
 
 function [sorted_list, last_inner, last_dirichlet] = point_sorter(glob, model)
-temp_inner = [];
-temp_dirichlet = [];
-temp_neumann = [];
-for x = glob
-    switch model.boundary_type(x)
-        case 0
-            temp_inner.append(x)
-        case -1
-            temp_dirichlet.append(x)
-        case -2
-            temp_neumann.append(x)
-    end
-end
-last_inner = size(temp_inner(1));
-last_dirichlet = size(temp_dirichlet(1)) + last_inner;
-sorted_list = [temp_inner; temp_dirichlet; temp_neumann];
+case_array = model.boundary_type(glob);
+temp_inner = find(case_array==0);
+temp_dirichlet = find(case_array==-1);
+temp_neumann = find(case_array==-2);
+%last_inner = size(temp_inner(1));
+last_inner = length(temp_inner);
+last_dirichlet = length(temp_dirichlet) + last_inner;
+sorted_list = [glob(temp_inner,:); glob(temp_dirichlet,:); glob(temp_neumann,:)];
 end
