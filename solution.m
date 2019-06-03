b5c7888a0e2a33d141a129d81585ee884da435df
Lennart <lennart.duvenbeck@gmail.com>
2019-06-03 18:31:23 +0200

    Assemblierung und Lösung

--- solution.m
+++ solution.m
@@ -1,12 +1,13 @@
 n = 500;
 m = 200;
 discr = diskretisierung(n, m);
-model = praktikum_elliptisch_model(0.5);
+model = praktikum_elliptisch_model(1);
 
 [glob, last_inner, last_dirichlet] = point_sorter(discr.Xh_strich, model);
-disp(last_inner);
-disp(last_dirichlet);
+% disp(last_inner);
+% disp(last_dirichlet);
 
+figure(1)
 plot(glob(1:last_inner, 1), glob(1:last_inner, 2), 'r.');
 hold on
 plot(glob(last_inner+1:last_dirichlet,1), glob(last_inner+1:last_dirichlet,2), 'b.');
@@ -14,17 +15,37 @@ hold on
 plot(glob(last_dirichlet+1:end,1), glob(last_dirichlet+1:end,2), 'g.');
 
 [A, b] = lgs_assembler(model, discr, last_inner, last_dirichlet);
+c = A\b;
+
+grid_width_temp = 100;
+X = linspace(-1, 1, grid_width_temp);
+Y = linspace(-1, 1, grid_width_temp);
+R = zeros(grid_width_temp, grid_width_temp);
+S = zeros(grid_width_temp, grid_width_temp);
+for i = 1:grid_width_temp
+    for j = 1:grid_width_temp
+        R(i,j) = sum(c .* discr.eval([X(i), Y(j)]));
+        S(i,j) = model.solution([X(i), Y(j)]);
+    end
+end
+figure(2)
+surf(X,Y,R);
+figure(3)
+surf(X,Y,S);
+
 
 function [A, b] = lgs_assembler(model, discr, last_inner, last_dirichlet)
 A = zeros(length(discr.Xh),length(discr.Xh_strich));
 for i = 1:last_inner
-    TODO
+    temp_func = @(x)(model.diffusivity(x) * discr.grad(discr.eval, x));
+    A(i,:) = -discr.div(temp_func, discr.Xh_strich(i,:));
 end
 for i = last_inner+1:last_dirichlet
     A(i,:) = discr.eval(discr.Xh_strich(i,:));
 end
 for i = last_dirichlet+1:length(discr.Xh_strich)
-    A(i,:) = model.diffusivity(discr.Xh_strich(i,:))*discr.grad(discr.eval, discr.Xh_strich(i,:))*model.normals(discr.Xh_strich(i,:));
+    temp = discr.grad(discr.eval, discr.Xh_strich(i,:)).*model.normals(discr.Xh_strich(i,:));
+    A(i,:) = model.diffusivity(discr.Xh_strich(i,:))*(temp(:,1)+temp(:,2));
 end
 b1 = model.source(discr.Xh_strich(1:last_inner,:));
 b2 = model.dirichlet_values(discr.Xh_strich(last_inner+1:last_dirichlet,:));
@@ -34,11 +55,7 @@ end
 
 function [sorted_list, last_inner, last_dirichlet] = point_sorter(glob, model)
 case_array = model.boundary_type(glob);
-temp_inner = find(case_array==0);
-temp_dirichlet = find(case_array==-1);
-temp_neumann = find(case_array==-2);
-%last_inner = size(temp_inner(1));
-last_inner = length(temp_inner);
-last_dirichlet = length(temp_dirichlet) + last_inner;
-sorted_list = [glob(temp_inner,:); glob(temp_dirichlet,:); glob(temp_neumann,:)];
+last_inner = length(glob(case_array==0));
+last_dirichlet = length(glob(case_array==-1)) + last_inner;
+sorted_list = [glob(case_array==0,:); glob(case_array==-1,:); glob(case_array==-2,:)];
 end
