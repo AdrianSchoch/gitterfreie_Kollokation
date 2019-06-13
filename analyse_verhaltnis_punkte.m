e5931f833a043b340ff27d3762ca3831517a83f4
Lennart <lennart.duvenbeck@gmail.com>
2019-06-13 13:14:36 +0200

    Verhältnis von inneren zu äußeren Punkten direkt analysiert

---
+++ analyse_verhaltnis_punkte.m
@@ -0,0 +1,19 @@
+model = praktikum_elliptisch_model(0.5);
+x = 100;
+L2_error_array = zeros(x, 10);
+Linf_error_array = zeros(x, 10);
+for i = 1:x
+    disp("i = " + num2str(i))
+    for j = 1:10
+        discr = diskretisierung(i, x - i, 0, model, 0.1);
+        [A, b] = lgs_assembler(model, discr);
+        c = A\b;
+        [N, S, L1_error, L2_error_array(i, j), Linf_error_array(i, j)] = grid_evaluation(100, c, discr, model);
+    end
+end
+figure(1)
+plot(linspace(1, x, x), median(L2_error_array.'));
+ylim([0 1])
+figure(2)
+plot(linspace(1, x, x), median(Linf_error_array.'));
+ylim([0 1])
